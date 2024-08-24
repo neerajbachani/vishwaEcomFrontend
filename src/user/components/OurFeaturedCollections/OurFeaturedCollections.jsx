7e4925c4f725688a719ed06873c7f8706b89b87a
neerajbachani <neerajbachani2003@gmail.com>
2024-08-24 20:51:35 +0530

    24-08-24 (2)

--- src/user/components/OurFeaturedCollections/OurFeaturedCollections.jsx
+++ src/user/components/OurFeaturedCollections/OurFeaturedCollections.jsx
@@ -28,12 +28,12 @@ const OurFeaturedCollections = () => {
         <div className='py-5 grid md:grid-cols-3 grid-cols-2 gap-5' >
           <div className='flex flex-col gap-2'>
             {featuredProducts.length > 0 && (
-              <DirectionAwareHover imageUrl={featuredProducts[0].image} className='w-full h-[16rem] object-cover'>
+              <DirectionAwareHover imageUrl={featuredProducts[0].image} className='w-full md:h-[16rem] h-[10rem] object-cover'>
                 <p>{featuredProducts[0].title}</p>
               </DirectionAwareHover>
             )}
             {featuredProducts.length > 1 && (
-              <DirectionAwareHover imageUrl={featuredProducts[1].image} className='w-full h-[16rem] object-cover'>
+              <DirectionAwareHover imageUrl={featuredProducts[1].image} className='w-full md:h-[16rem] h-[10rem]  object-cover'>
                 
                 <p>{featuredProducts[1].title}</p>
               </DirectionAwareHover>
@@ -41,7 +41,7 @@ const OurFeaturedCollections = () => {
           </div>
           <div>
             {featuredProducts.length > 2 && (
-              <DirectionAwareHover imageUrl={featuredProducts[2].image} className='w-full h-[32.5rem] object-cover'>
+              <DirectionAwareHover imageUrl={featuredProducts[2].image} className='w-full md:h-[32.5rem] h-[20.25rem] object-cover'>
               
                 <p>{featuredProducts[2].title}</p>
               </DirectionAwareHover>
@@ -49,12 +49,12 @@ const OurFeaturedCollections = () => {
           </div>
           <div className='grid grid-flow-col md:flex md:flex-col col-span-2 md:col-span-1 md:gap-2 gap-5'>
             {featuredProducts.length > 3 && (
-              <DirectionAwareHover imageUrl={featuredProducts[3].image} className='w-full h-[16rem] object-cover'>
+              <DirectionAwareHover imageUrl={featuredProducts[3].image} className='w-full md:h-[16rem] h-[10rem] object-cover'>
                 <p>{featuredProducts[3].title}</p>
               </DirectionAwareHover>
             )}
             {featuredProducts.length > 4 && (
-              <DirectionAwareHover imageUrl={featuredProducts[4].image}  className='w-full h-[16rem] object-cover'>
+              <DirectionAwareHover imageUrl={featuredProducts[4].image}  className='w-full md:h-[16rem] h-[10rem]  object-cover'>
                 <p>{featuredProducts[4].title}</p>
               </DirectionAwareHover>
             )}
